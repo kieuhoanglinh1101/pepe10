ce78bfc43af6d87cc24667cabfd297db30ad0ebc
kieuhoanglinh1101 <kieuhoanglinh1101@gmail.com>
2026-07-30 12:57:06 +0700

    Updated Impressions.tsx

--- src/components/Impressions.tsx
+++ src/components/Impressions.tsx
@@ -4,7 +4,7 @@ import { useReveal } from '@/hooks/useReveal';
 // ─── Image assets ────────────────────────────────────────────────────────────
 const CARD_IMAGES = [
   'https://ik.imagekit.io/zznoau6lx/bee56bec-991e-4e14-9f93-dbd941924657.png?updatedAt=1784945876969',
-  'https://ik.imagekit.io/zznoau6lx/Cybercoin%20webp/2/MV5BODU3NTA3ZWQtNDkyZi00NjM2LWI1NDUtNjBlNGVmZmQ0NGZjXkEyXkFqcGc@._V1_.webp?updatedAt=1785252491298',
+  'https://ik.imagekit.io/zznoau6lx/b4084e0f-7c91-4aa7-8a87-cba98fa11496.png?updatedAt=1784945877231',
   'https://ik.imagekit.io/zznoau6lx/Cybercoin%20webp/2/rebecca.webp?updatedAt=1785252491273',
   'https://ik.imagekit.io/zznoau6lx/Cybercoin%20webp/2/wp11496855_mPEmctV-P.webp?updatedAt=1785252491248',
   'https://ik.imagekit.io/zznoau6lx/Cybercoin%20webp/2/david2.webp?updatedAt=1785251906096',
